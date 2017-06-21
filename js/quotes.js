cebea57346f41f8b341dbc4b2d82ba9521e08054
Malte Niepel <malteniepel@Catawikis-MacBook-Air-6.local>
2017-06-21 11:55:50 +0200

    ES6 updated

--- js/quotes.js
+++ js/quotes.js
@@ -1,17 +1,32 @@
 var quotes = [
 	{
-		quote : 'Only two things are infinite, the universe and human stupidity, and I&#39;m not sure about the former.', source: 'Albert Einstein', citation: '', year: '1879-1955'
+		quote : 'Only two things are infinite, the universe and human stupidity, and I&#39;m not sure about the former.',
+		source: 'Albert Einstein',
+		citation: '',
+		year: '1879-1955'
 	},
 	{
-		quote : 'A lie gets halfway around the world before the truth has a chance to get its pants on.', source: 'Sir Winston Churchill', citation: '', year: ''
+		quote : 'A lie gets halfway around the world before the truth has a chance to get its pants on.',
+		source: 'Sir Winston Churchill',
+		citation: '',
+		year: '1874-1965'
 	},
 	{
-		quote : 'I find that the harder I work, the more luck I seem to have.', source: 'Thomas Jefferson', citation: '', year: '1743-1826'
+		quote : 'I find that the harder I work, the more luck I seem to have.',
+		source: 'Thomas Jefferson',
+		citation: '',
+		year: '1743-1826'
 	},
 	{
-		quote : 'Any man who is under 30, and is not a liberal, has no heart; and any man who is over 30, and is not a conservative, has no brains.', source: 'Sir Winston Churchill', citation: '', year: '1874-1965'
+		quote : 'Any man who is under 30, and is not a liberal, has no heart; and any man who is over 30, and is not a conservative, has no brains.',
+		source: 'Sir Winston Churchill',
+		citation: '',
+		year: '1874-1965'
 	},
 	{
-		quote : 'Few things are harder to put up with than a good example.', source: 'Mark Twain', citation: '', year: '1835-1910'
+		quote : 'Few things are harder to put up with than a good example.',
+		source: 'Mark Twain',
+		citation: '',
+		year: '1835-1910'
 	}
 ];
--- js/script.js
+++ js/script.js
@@ -1,79 +1,65 @@
+(function(){
+	//calls printQuote when the page loads
 
-//calls printQuote when the page loads
+	// event listener to trigger the first quote when the page loads
+	window.addEventListener('load', printQuote, false);
 
-
-
-// event listener to trigger the first quote when the page loads
-window.addEventListener('load', printQuote, false);
-
-// event listener to respond to clicks on the page
-// when user clicks anywhere on the page, the "makeQuote" function is called
-document.getElementById('loadQuote').addEventListener("click", printQuote, false);
+	// event listener to respond to clicks on the page
+	// when user clicks anywhere on the page, the "makeQuote" function is called
+	document.getElementById('loadQuote').addEventListener("click", printQuote, false);
 
 
-//all variables
-var clone = new Array(),
-	intervalId;
+	//all variables
+	var clone = new Array(),
+		intervalId;
 
 
+	//resets the extra arrays after ALL quotes have been displayed
+	const reset = () => clone = quotes.slice();
 
-//resets the extra arrays after ALL quotes have been displayed
-function reset() {
-    clone = quotes.slice();
-};
+	//New qoute generated after timeout
+	const refreshTimer = () => intervalId  = setTimeout(printQuote, 30000);
 
-//New qoute generated after timeout
-
-function refreshTimer() {
-	return intervalId  = setTimeout(printQuote, 30000);
-}
-
-//returns the randomly selected quote object
-//needs to return the object of i before splicing i from the array - how to do it?
-function getQuote() {
-    if (clone.length === 0) {
-        reset();
-    }
-    return clone.splice(getRandomIndex(clone),1).pop();        
-}
-
-//selects a random quote object from the quotes array
-function getRandomIndex(array) {	
-return Math.floor(Math.random()* array.length );
-}
-
-
-
-function getRandomRGB() {
-	var red = Math.floor(Math.random()* 256);
-	var green = Math.floor(Math.random()* 256);
-	var blue = Math.floor(Math.random()* 256);
-	return rgbColor = 'rgb(' + red + ',' + green + ',' + blue + ')';
-}
-
-
-
-//calls getRandomQuote function and stores it in a variable
-function printQuote() {
-	var quote = getQuote();
-	var backgroundRGB = getRandomRGB();
-	var message = '';
-
-	message += '<p class="quote">' + quote.quote + '</p>';
-	message += '<p class="source">' + quote.source + '';
-	if (quote.citation !== '') {
-		message += '<span class="citation">' + quote.citation + '</span>';
+	//returns the randomly selected quote object
+	//needs to return the object of i before splicing i from the array - how to do it?
+	const getQuote = () => {
+	    if (clone.length === 0) {
+	        reset();
+	    }
+	    return clone.splice(getRandomIndex(clone), 1).pop();
 	}
-	if (quote.year !== '') {
-		message += '<span class="year">' + quote.year + '</span>';
+
+	//selects a random quote object from the quotes array
+	const getRandomIndex = (array) => Math.floor(Math.random()* array.length );
+
+	function getRandomRGB() {
+		const red = Math.floor(Math.random()* 256);
+		const green = Math.floor(Math.random()* 256);
+		const blue = Math.floor(Math.random()* 256);
+		return rgbColor = 'rgb(' + red + ',' + green + ',' + blue + ')';
 	}
-	message += '</p>';
-
-	document.getElementById('quote-box').innerHTML = message;
-	document.body.style.backgroundColor = backgroundRGB;
-	window.clearTimeout(intervalId);
-	refreshTimer();
-}
 
 
 
+	//calls getRandomQuote function and stores it in a variable
+	function printQuote() {
+		const quote = getQuote();
+		const backgroundRGB = getRandomRGB();
+		var message = '';
+
+		message += '<p class="quote">' + quote.quote + '</p>';
+		message += '<p class="source">' + quote.source + '';
+		if (quote.citation !== '') {
+			message += '<span class="citation">' + quote.citation + '</span>';
+		}
+		if (quote.year !== '') {
+			message += '<span class="year">' + quote.year + '</span>';
+		}
+		message += '</p>';
+
+		document.getElementById('quote-box').innerHTML = message;
+		document.body.style.backgroundColor = backgroundRGB;
+		window.clearTimeout(intervalId);
+		refreshTimer();
+	}
+}())
